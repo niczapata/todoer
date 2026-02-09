5bd1277face38a3992f889ce6da7739857f9b9ef
Nicolas Zapata <nicozapata@msn.com>
2026-02-09 16:53:30 -0500

    feat(todo): add update and delete functionality

--- todo/todo.py
+++ todo/todo.py
@@ -38,13 +38,45 @@ def create():
     return render_template("todo/create.html")
 
 
+def get_todo(id):
+    db, c = get_db()
+    c.execute(
+        "SELECT t.id, t.description, t.username, t.completed, t.created_at, t.created_by",
+        "FROM todo t JOIN USER u on t.created_by = u.id WHERE t.id = %s",
+        (id,),
+    )
+    todo = c.fetchone()
+    if todo is None:
+        abort(404, f"Todo id {id} does not exist.")
+    return todo
+
+
 @bp.route("/<int:id>/update", methods=("GET", "POST"))
 @login_required
 def update(id):
-    return render_template("todo/update.html")
+    todo = get_todo(id)
+    if request.method == "POST":
+        description = request.form["description"]
+        completed = True if request.form.get("completed") == "on" else False
+        error = None
+        if not description:
+            error = "Description is required."
+        if error is None:
+            db, c = get_db()
+            c.execute(
+                "UPDATE todo SET description = %s, completed = %s WHERE id = %s",
+                (description, completed, id),
+            )
+            db.commit()
+            return redirect(url_for("todo.index"))
+        flash(error)
+    return render_template("todo/update.html", todo=todo)
 
 
 @bp.route("/<int:id>/delete", methods=("POST"))
 @login_required
-def delete():
-    return ""
+def delete(id):
+    db, c = get_db()
+    c.execute("DELETE FROM todo WHERE id = %s", (id,))
+    db.commit()
+    return redirect(url_for("todo.index"))
